8bf9dccdeacffe17675a967ab2c42f09c52fde96
David Yunusa <daveed856@outlook.com>
2023-01-19 10:20:00 +0100

    Add process component

--- components/Process.jsx
+++ components/Process.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import styled from 'styled-components';
-import notes from '../../public/images/notes.svg';
+import Notes from '../assets/images/Notes.jsx';
 
 const ProcessCont = styled.div`
   display: inline-flex;
@@ -14,6 +14,14 @@ const ProcessCont = styled.div`
   &:hover {
     box-shadow: 0px 12px 64px rgba(28, 25, 25, 0.12);
   }
+
+  &:hover div:first-child {
+    background-color: ${(props) => props.frameBg};
+    color: #fff;
+  }
+  &:hover path {
+    fill: #fff;
+  }
 `;
 const Frame = styled.div`
   background-color: #edd8ffff;
@@ -22,27 +30,26 @@ const Frame = styled.div`
 `;
 
 const Heading3 = styled.h3`
-  font-family: 'Work Sans', sans-serif;
   font-size: 20px;
   font-weight: 600;
   line-height: 1.5;
 
   color: ${(props) => props.color};
-  margin: 16px 0px;
+  margin: 1rem 0 12px 0;
 `;
 
 const Text = styled.p`
   color: ${(props) => props.color};
   font-weight: 400;
-  font-size: 16px;
+  font-size: 1rem;
   margin: 0;
 `;
 
 export default function Process({ c1, c2, c3, title, text }) {
   return (
-    <ProcessCont>
+    <ProcessCont frameBg={c1}>
       <Frame>
-        <img src={notes} alt="note" />
+        <Notes />
       </Frame>
       <Heading3 color={c3}>{title}</Heading3>
       <Text color={c2}>{text}</Text>
